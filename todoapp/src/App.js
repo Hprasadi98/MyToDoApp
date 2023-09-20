460242e407f2b8dcd86670a3cc6203dfaa9ef994
Prasadini V.G.H <prasadinivgh.21@uom.lk>
2023-09-20 19:16:11 +0530

    delete

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,20 +1,7 @@
 import React from 'react';
 import './App.css';
-import todo from './images/todo.png';
-import TodoList from './components/TodoList';
 
 function App() {
-  return (
-    <div className='todo'>
-      <div className='header'>
-      <p>To-Do List</p>
-      <img src={todo} alt=''></img>
-      
-      </div>
-      <TodoList/>
-      
-      
-    </div>
-  );
+  return ;
 }
 export default App;
